1cb00d4ed711d02e0284e8efe8d513922a8ffc84
Thomas Taege <thomas.taege@gmail.com>
2014-06-02 14:11:57 +1000

    Regenerated rails folder with rails 4.1.1, removed assets pipeline and added active_model_serializers in Rails

--- rails/config/environment.rb
+++ rails/config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-Bostonember::Application.initialize!
+Rails.application.initialize!
--- rails/config/initializers/session_store.rb
+++ rails/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Bostonember::Application.config.session_store :cookie_store, key: '_bostonember_session'
+Rails.application.config.session_store :cookie_store, key: '_bostonember_session'
